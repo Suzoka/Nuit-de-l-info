4aa6738e05c73e87c0c63c14f07dc54abb554c44
Raphi04 <raphaelcadete04@gmail.com>
2023-12-08 04:50:43 +0100

    quizz

--- src/scripts/quizz.js
+++ src/scripts/quizz.js
@@ -1,10 +1,8 @@
 function moreInfo(info, state){
-    console.log("coucou");
-    var infoBulle = document.getElementById(info);
+    let infoBulle = document.getElementById(info);
     if (state == "open") {
         infoBulle.style.display = "flex";
     } else {
         infoBulle.style.display = "none";
     }
 }
-
