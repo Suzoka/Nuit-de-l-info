4773f5ef34c4179beb24c1f2ae60d23b7c16657e
Raphi04 <raphaelcadete04@gmail.com>
2023-12-08 03:56:38 +0100

    ^^

--- src/scripts/quizz.js
+++ src/scripts/quizz.js
@@ -29,3 +29,12 @@ function verif(e) {
         //fonction mauvaise réponse
     }
 }
+
+function moreInfo(info, state){
+    let info = document.getElementById(info);
+    if (state == "open") {
+        info.style.display = "flex";
+    } else {
+        info.style.display = "none";
+    }
+}
